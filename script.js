1e6b6d05ea015b8220535d0416053c04ce4b63dc
unagatacuriosa <gabrielacanalesrubio@gmail.com>
2023-03-01 14:14:09 +0100

    Pequeños retoques

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 // Selecciona los elementos del DOM que se van a utilizar en el código
-const modal = document.querySelector('.modal')
-const overlay = document.querySelector('.overlay')
-const btnCloseModal = document.querySelector('.close-modal')
-const btnOpenModal = document.querySelector('.show-modal')
+const MODAL = document.querySelector('.modal')
+const OVERLAY = document.querySelector('.overlay')
+const BTNCLOSEMODAL = document.querySelector('.close-modal')
+const BTNOPENMODAL = document.querySelectorAll('.show-modal')
 
 // Define una función para abrir el modal
-const openModal = function () {
-  modal.classList.remove('hidden')
-  overlay.classList.remove('hidden')
+const openModal = function (e) {
+  if (e.target.classList.contains('show-modal')) {
+    MODAL.classList.remove('hidden')
+    OVERLAY.classList.remove('hidden')
+  }
 }
 
 // Define una función para cerrar el modal
 const closeModal = function () {
-  modal.classList.add('hidden')
-  overlay.classList.add('hidden')
+  MODAL.classList.add('hidden')
+  OVERLAY.classList.add('hidden')
 }
 
 // Usando un foreach en vez de un for
-// btnOpenModal.forEach((btn) => btn.addEventListener('clicl', openModal))
-
-// Agrega un event listener a cada botón de "Abrir modal" usando un bucle for
-for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener('click', openModal)
+BTNOPENMODAL.forEach((btn) => btn.addEventListener('click', openModal))
 
 // Agrega un event listener al botón de "Cerrar modal"
-btnCloseModal.addEventListener('click', closeModal)
+BTNCLOSEMODAL.addEventListener('click', closeModal)
 
 // Agrega un event listener al overlay del modal para cerrar el modal cuando se hace clic fuera de él
-overlay.addEventListener('click', closeModal)
+OVERLAY.addEventListener('click', closeModal)
 
 // Agrega un event listener para cerrar el modal cuando se presiona la tecla "Esc"
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+  if (e.key === 'Escape' && !MODAL.classList.contains('hidden')) {
     closeModal()
   }
+  e.stopPropagation
 })
